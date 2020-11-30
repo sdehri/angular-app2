0e66a8273f02906168be0fbc77b96ff93fcee095
sdehri <71267763+sdehri@users.noreply.github.com>
2020-11-30 22:59:25 +0530

    Add files via upload

---
+++ routerjs/main.js
@@ -0,0 +1,14 @@
+
+var mainApp = angular.module("mainApp", ['ngRoute']);
+
+mainApp.config(function($routeProvider) {
+	/* Add code here */
+});
+
+mainApp.controller('UserController', function($scope) {
+	/* Add code here */
+});
+
+mainApp.factory('user', ['$http', function($http) {
+   /* Add code here */
+}]);
